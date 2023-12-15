9dcaffca4b5fb85a237fee6dce7a0bd3c02b2ac9
Karol kalololol <karol.binkowski92@gmail.com>
2023-12-15 21:49:26 +0100

    Revert "module-5 tests"
    This reverts commit 30b4ab3d837700f7164fbbe071b33d5fe505fc8d.

--- src/CheckText.java
+++ src/CheckText.java
@@ -1,23 +1,11 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class CheckText {
     //Napisz program, który sprawdzi, czy 2 pliki tekstowe mają taką samą zawartość:
     //public boolean areTextFilesEqual(String filePath1, String filePath2);
 
-    private String filePath;
-    private String filePath1;
-    private String filePath2;
-    public CheckText(){}
-    public CheckText(String filePath){
-        this.filePath = filePath;
-    }
-    public CheckText(String filePath1, String filePath2 ){
-        this.filePath1 = filePath1;
-        this.filePath2 = filePath2;
-    }
     public boolean areTextFilesEqual(String filePath1, String filePath2) {
 
         try {
@@ -48,24 +36,12 @@ public class CheckText {
         boolean result = false;
         try {
             BufferedReader file = new BufferedReader(new FileReader(filepath));
-            String input = "";
-            String readLine = "";
-            List<String> newList = new LinkedList<>();
-            while ( (readLine = file.readLine()) != null) {
-                newList.add(readLine);
+            String input;
+            int lineCount = 0;
+            while (file.readLine() != null) {
+                lineCount++;
             }
-
-                File plik = new File("nowyPlik.txt");
-                FileWriter fWriter = new FileWriter(plik);
-
-                for (int i = 0; i < newList.size(); i++) {
-                    int num = newList.size() - i - 1;
-                    input = newList.get(num);
-                    fWriter.write(input + "\n");
-                }
-                fWriter.close();
-                result = true;
-
+            String[] newLines = new String[lineCount];
 
         }catch (IOException e){
             System.out.println("Błąd");
@@ -74,29 +50,5 @@ public class CheckText {
         return result;
     }
 
-    public boolean invertFile1(String filepath) {
-        boolean result = false;
-        try {
-            BufferedReader file = new BufferedReader(new FileReader(filepath));
-            String input1 = file.readLine();
-            String[] tabTextFile = input1.split(" ");
-
-            File plik = new File("nowyPlik.txt");
-            FileWriter fWriter = new FileWriter(plik);
-
-            for(int i = 0; i < tabTextFile.length; i++){
-                int num = tabTextFile.length - i - 1;
-                fWriter.write(tabTextFile[num] + " ");
-            }
-            fWriter.close();
-            result = true;
-
-
-        } catch (IOException e) {
-            System.out.println("Błąd");
-        }
-
-        return result;
-    }
 
 }
